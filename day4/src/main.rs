cb91292ebf292ad0095590bcb50cc797c1944292
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-12-04 22:30:30 +0100

    add (very manual) solution for part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    fs::{read_to_string, File},
-    io::{self, BufRead, BufReader},
-};
+use std::fs::read_to_string;
 
 fn main() {
     let entry = read_to_string("data.txt").unwrap();
@@ -15,22 +12,42 @@ fn main() {
                 .map(|str| str.parse::<u32>().unwrap_or(0))
                 .collect::<Vec<_>>()
         })
-        .fold(0, |mut acc: i32, vec| {
-            let first_range = &vec[0..2];
-            let second_range = &vec[2..4];
-            if first_range[0] > second_range[0] {
-                if first_range[1] <= second_range[1] {
-                    acc += 1;
-                }
-            } else if first_range[0] < second_range[0] {
-                if first_range[1] >= second_range[1] {
-                    acc += 1;
-                }
-            } else {
-                acc += 1;
-            }
-            
-            acc
-        });
+        .fold(0, part2);
     println!("Result: {result}");
+}
+
+fn part1(mut acc: i32, vec: Vec<u32>) -> i32 {
+    let first_range = &vec[0..2];
+    let second_range = &vec[2..4];
+    if first_range[0] > second_range[0] {
+        if first_range[1] <= second_range[1] {
+            acc += 1;
+        }
+    } else if first_range[0] < second_range[0] {
+        if first_range[1] >= second_range[1] {
+            acc += 1;
+        }
+    } else {
+        acc += 1;
+    }
+
+    acc
+}
+
+fn part2(mut acc: i32, vec: Vec<u32>) -> i32 {
+    let first_range = &vec[0..2];
+    let second_range = &vec[2..4];
+    if first_range[0] < second_range[0] {
+        if first_range[1] >= second_range[0] {
+            acc += 1;
+        }
+    } else if first_range[0] > second_range[0] {
+        if first_range[0] <= second_range[1] {
+            acc += 1;
+        }
+    } else {
+        acc += 1;
+    }
+
+    acc
 }
