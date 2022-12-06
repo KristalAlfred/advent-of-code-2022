1b2ac3a70d59c9b1d502f5f822e40913d0aec662
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-12-06 22:47:48 +0100

    touch ups

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,7 +3,7 @@ use std::{collections::HashSet, fs::read_to_string};
 fn main() {
     let data = read_to_string("data.txt");
 
-    let start_of_message_counter = 14;
+    let start_of_message_counter = 14; // 4 for part 1 solution.
 
     let mut result = 0;
     if let Ok(content) = data {
@@ -14,8 +14,8 @@ fn main() {
                                             .enumerate() {
             {
                 let mut map = HashSet::new();
-                let is_unique = win.into_iter().all(move |element| map.insert(element));
-                if is_unique {
+                let is_unique_sequence = win.into_iter().all(move |element| map.insert(element));
+                if is_unique_sequence {
                     // 0-indexed and the challenge is asking for the ending index, i is the starting one.
                     result = i + start_of_message_counter;
                     break;
