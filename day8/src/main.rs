78bd564c2c1279f00d07798cfff24ea1bc392c56
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-12-08 12:38:09 +0100

    add solution for part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -10,15 +10,19 @@ fn main() {
             if bool { acc += 1 }
             acc
         });
-        println!("Result: {result}")
+        println!("Trees visible from the outside: {result}")
     }
 }
 
 fn get_visibility_grid(grid: &Vec<Vec<u32>>) -> Vec<Vec<bool>> {
     let mut visibility_grid = vec![vec![true; grid.len()]; grid.len()];
 
+    let mut maximum_scenic_score = 0;
+
     for (row_number, row) in grid.iter().enumerate() {
         for (column_number, &cell_value) in row.iter().enumerate() {
+            let mut scenic_score = 1;
+
             // All trees are visible at the edges
             if row_number == 0
                 || row_number == grid.len() - 1
@@ -30,51 +34,63 @@ fn get_visibility_grid(grid: &Vec<Vec<u32>>) -> Vec<Vec<bool>> {
 
             let mut visible_in_any_direction = [true, true, true, true];
             for dir in 0..4 {
+                let mut local_scenic_score = 0;
                 match dir {
                     0 => {
                         // Check north
-                        for position in 0..row_number {
+                        for position in (0..row_number).rev() {
+                            local_scenic_score += 1;
                             if grid[position][column_number] >= cell_value {
                                 visible_in_any_direction[0] = false;
                                 break;
                             }
                         }
+                        scenic_score *= local_scenic_score;
                     },
                     1 => {
                         // Check east
                         for position in column_number..grid.len() {
                             if position == column_number { continue; }
+                            local_scenic_score += 1;
                             if grid[row_number][position] >= cell_value {
                                 visible_in_any_direction[1] = false;
                                 break;
                             }
                         }
+                        scenic_score *= local_scenic_score;
                     },
                     2 => {
                         // Check south
+                        
                         for position in row_number..grid.len() {
                             if position == row_number { continue; }
+                            local_scenic_score += 1;
                             if grid[position][column_number] >= cell_value {
                                 visible_in_any_direction[2] = false;
                                 break;
                             }
                         }
+                        scenic_score *= local_scenic_score;
                     },
                     3 => {
                         // Check west
-                        for position in 0..column_number {
+                        for position in (0..column_number).rev() {
+                            local_scenic_score += 1;
                             if grid[row_number][position] >= cell_value {
                                 visible_in_any_direction[3] = false;
                                 break;
                             }
                         }
+                        scenic_score *= local_scenic_score;
                     }
                     _ => unreachable!()
                 }
             }
             visibility_grid[column_number][row_number] = visible_in_any_direction.iter().any(|&b| b);
+            maximum_scenic_score = std::cmp::max(scenic_score, maximum_scenic_score);
         }
     }
+    println!("Maximum scenic score: {maximum_scenic_score}");
 
     visibility_grid
 }
